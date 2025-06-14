e801850d5a8dec6f9bda0490bc9828722b5ed812
Ymgaa <yeremiaegasn@gmail.com>
2025-06-14 11:50:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>To-Do List dengan Checkbox</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>To-Do List with Checkbox</title>
+   <body>
+    <div class="container">
+        <h2>📝 To-Do List (Checkbox)</h2>
+
+        <!-- Form tambah tugas -->
+        <form method="POST" class="add-form">
+            <input type="text" name="task" placeholder="Tulis tugas baru..." required>
+            <button type="submit">Tambah</button>
+        </form>
+
+        <!-- Daftar tugas -->
+        <ul>
+            
+        </ul>
+    </div>
+</body>
+</html>
